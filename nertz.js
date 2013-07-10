fcb5f058470d872eedd02a06e0351c1aa5ff7b2c
OpenShift guest <cletusw@gmail.com>
2013-07-10 03:03:46 -0400

    Two players

--- nertz.js
+++ nertz.js
@@ -1,5 +1,6 @@
 var lake = [];
 var player1 = new ComputerPlayer(lake);
+var player2 = new ComputerPlayer(lake);
 
 var intervalId;
 
@@ -13,7 +14,22 @@ function stop() {
 }
 
 function takeTurn() {
-    player1.takeTurn();
+    try {
+        player1.takeTurn();
+    } catch (e) {
+        stop();
+        console.log('Player 1 Wins!');
+        debug();
+        return;
+    }
+    try {
+        player2.takeTurn();
+    } catch (e) {
+        stop();
+        console.log('Player 2 Wins!');
+        debug();
+        return;
+    }
     debug();
 }
 
@@ -22,7 +38,7 @@ function debug() {
     for (var i = 0; i < lake.length; ++i) {
         lakeSize += lake[i].length;
     }
-    if (lakeSize + player1.debugNumCards() !== 1 * 56) {
+    if (lakeSize + player1.debugNumCards() + player2.debugNumCards() !== 2 * 56) {
         throw new Error('Invalid number of cards in play');
     }
 
@@ -33,4 +49,5 @@ function debug() {
         console.log(pile[pile.length - 1]);
     }
     player1.debug();
+    player2.debug();
 }
